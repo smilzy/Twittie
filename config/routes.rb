4d0390439116e8bacb7456f677324a4dea42ef9d
Smilzy <4wozniak@gmail.com>
2017-06-11 23:37:54 +0000

    Added contacts form controller, view, route

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :account_activations,   only: [:edit]
   resources :password_resets,       only: [:new, :create, :edit, :update]
   resources :microposts,            only: [:create, :destroy]
+  resources :contacts,              only: :create
 end
