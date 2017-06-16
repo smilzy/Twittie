00914a6b6f07abe56bd7f7e7f5fe5b01520bc879
Smilzy <4wozniak@gmail.com>
2017-06-16 20:29:38 +0000

    Added display of followers/following count and follow/unfollow buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,21 @@ Rails.application.routes.draw do
   root                    'static_pages#home'
   get     'help',     to: 'static_pages#help'
   get     'about',    to: 'static_pages#about'
-  get     'cv',    to:    'static_pages#cv'
+  get     'cv',       to: 'static_pages#cv'
   get     'signup',   to: 'users#new'
   post    'signup',   to: 'users#create'
   get     'login',    to: 'sessions#new'
   post    'login',    to: 'sessions#create'
   delete  'logout',   to: 'sessions#destroy'
-  resources :users
+  get     'contact',  to: 'contacts#new', as: 'new_contact'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations,   only: [:edit]
   resources :password_resets,       only: [:new, :create, :edit, :update]
   resources :microposts,            only: [:create, :destroy]
   resources :contacts,              only: :create
-  get 'contact', to: 'contacts#new', as: 'new_contact'
+  resources :relationships,         only: [:create, :destroy]
 end
